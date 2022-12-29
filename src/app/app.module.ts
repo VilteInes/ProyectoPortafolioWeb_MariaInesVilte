f390d471c1c8cfc1c79f09eb0413eab20535c99b
VilteInes <inesvilte1133@gmail.com>
2022-12-28 22:38:14 -0300

    Creacion del banner y el acerca de mi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
